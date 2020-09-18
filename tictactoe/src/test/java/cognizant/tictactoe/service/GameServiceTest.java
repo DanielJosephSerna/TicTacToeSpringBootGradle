5eae99174e07e7ddd3e6a48cfa31bc61f7f9cfac
DanielJosephSerna <daniel.srn5@gmail.com>
2020-09-17 20:22:58 -0500

    game service test was forgotten on last commit and push

---
+++ tictactoe/src/test/java/cognizant/tictactoe/service/GameServiceTest.java
@@ -0,0 +1,55 @@
+package cognizant.tictactoe.service;
+
+import cognizant.tictactoe.constants.GameConst;
+import cognizant.tictactoe.constants.PlayerConst;
+import cognizant.tictactoe.model.Board;
+import cognizant.tictactoe.model.Game;
+import cognizant.tictactoe.model.Player;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+class GameServiceTest {
+
+    private GameService gameServiceUnderTest;
+
+    @BeforeEach
+    void setup() {
+        gameServiceUnderTest = new GameService();
+    }
+
+    @Test
+    void whenBuildGameHumanComputerIsCalled_ThenReturnGameObject() {
+        // setup
+        Player player1 = new Player();
+        player1.setType(PlayerConst.HUMAN);
+        player1.setPiece(PlayerConst.X);
+        Player player2 = new Player();
+        player2.setType(PlayerConst.COMPUTER);
+        player2.setPiece(PlayerConst.O);
+        List<Player> playerList = new ArrayList<>();
+        playerList.add(player1);
+        playerList.add(player2);
+
+        char [] boardArr = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        int moveCount = 0;
+        Board board = new Board();
+        board.setBoardArr(boardArr);
+        board.setMoveCount(moveCount);
+
+        String stateOfPlay = GameConst.ONGOING;
+        String typeOfGame = GameConst.HUMAN_VS_COMPUTER;
+        Game game = new Game(playerList, board, stateOfPlay, typeOfGame);
+
+        // implement the test
+        // final Game result = gameServiceUnderTest.buildGameHumanComputer();
+
+        // verify the results
+        Assertions.assertEquals(game, gameServiceUnderTest.buildGameHumanComputer());
+    }
+}
